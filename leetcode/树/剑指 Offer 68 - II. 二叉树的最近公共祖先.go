b3f3419eb33bd5d0e6cb5ec08abd5456817434a4
seven <zhouwen@chelun.com>
2022-04-07 22:09:05 +0800

    提交每日一题

---
+++ leetcode/树/剑指 Offer 68 - II. 二叉树的最近公共祖先.go
@@ -0,0 +1,39 @@
+package 树
+
+/**
+题目：
+给定一个二叉树, 找到该树中两个指定节点的最近公共祖先。
+
+思路：
+递归
+*/
+
+// Definition for a binary tree node.
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	if root.Val == p.Val || root.Val == q.Val {
+		return root
+	}
+
+	left := lowestCommonAncestor(root.Left, p, q)
+	right := lowestCommonAncestor(root.Right, p, q)
+
+	//说明root的左右孩子分别是p q
+	if left != nil && right != nil {
+		return root
+	}
+
+	if left != nil {
+		return left
+	}
+
+	return right
+}
