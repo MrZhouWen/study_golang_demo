016596ab36f4577d64b33c1dcf58dd25a62a9586
seven <zhouwen@chelun.com>
2022-05-14 09:19:34 +0800

    本周每日一题题解整理

--- leetcode/二叉树/剑指 Offer 68 - II. 二叉树的最近公共祖先.go
+++ leetcode/二叉树/剑指 Offer 68 - II. 二叉树的最近公共祖先.go
@@ -19,18 +19,20 @@ func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
 	if root == nil {
 		return nil
 	}
+	//root也可以作为自己的祖先
 	if root.Val == p.Val || root.Val == q.Val {
 		return root
 	}
 
-	left := lowestCommonAncestor(root.Left, p, q)
+	left := lowestCommonAncestor(root.Left, p, q) //找寻，p或者q是否有一个、或者都在left子树中
 	right := lowestCommonAncestor(root.Right, p, q)
 
-	//说明root的左右孩子分别是p q
+	//说明p, q 分别在 root 的左右子树中，所以root为最近公共祖先
 	if left != nil && right != nil {
 		return root
 	}
 
+	//说明right == nil ,都在左子树上，最后返回的left就是最近祖先
 	if left != nil {
 		return left
 	}
