9778f90762f37a0fd4cb185c4d34d6005d404149
seven <zhouwen@chelun.com>
2022-06-24 14:57:29 +0800

    最近算法题整理

--- leetcode/main.go
+++ leetcode/main.go
@@ -1,10 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	var nums1 []interface{}
-	nums2 := []int{1, 3, 4}
-	nums3 := append(nums1, nums2)
-	fmt.Println(len(nums3))
+	var nums1 = []int{1, 2, 3}
+	nums2 := nums1[:2]
+
+	fmt.Println(math.MaxInt8 >> 1)
+
+	nums2 = append(nums2, 30)
+	fmt.Printf("num1=%v, num2=%v", nums1, nums2)
+	fmt.Printf("cap of num1 = %d; cap of num2 = %d \n\r", cap(nums1), cap(nums2))
+
+	nums2 = append(nums2, 50)
+	fmt.Printf("num1=%v, num2=%v", nums1, nums2)
+	fmt.Printf("cap of num1 = %d; cap of num2 = %d \n\r", cap(nums1), cap(nums2))
 }
